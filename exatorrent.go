0ee8a9e239239b5a5c3972a50e64c0ecb4a07e17
Evrins Hu <evrins.hu@gmail.com>
2024-03-08 12:38:10 +0800

    feat: add GetMagnet api

--- exatorrent.go
+++ exatorrent.go
@@ -14,6 +14,7 @@ import (
 func main() {
 	core.Initialize()
 
+	http.HandleFunc("GET /api/magnet", core.GetMagnet)
 	http.HandleFunc("POST /api/magnet", core.AddMagnet)
 	http.HandleFunc("DELETE /api/magnet", core.RemoveMagnet)
 	http.HandleFunc("/api/socket", core.SocketAPI)
--- internal/core/magnet.go
+++ internal/core/magnet.go
@@ -13,6 +13,11 @@ type AddMagnetRequest struct {
 	AutoStart bool   `json:"autoStart"`
 }
 
+func GetMagnet(w http.ResponseWriter, r *http.Request) {
+	var buf = GetTorrents(Engine.TUDb.ListTorrents("adminuser"))
+	handleResponseBytes(w, r, buf)
+}
+
 func AddMagnet(w http.ResponseWriter, r *http.Request) {
 	var req AddMagnetRequest
 	var err = json.NewDecoder(r.Body).Decode(&req)
@@ -72,5 +77,9 @@ func handleResponse(w http.ResponseWriter, r *http.Request, resp Resp) {
 	var err = json.NewEncoder(w).Encode(resp)
 	if err != nil {
 		slog.Info("fail to send response. err: %v", err)
+func handleResponseBytes(w http.ResponseWriter, r *http.Request, buf []byte) {
+	var _, err = w.Write(buf)
+	if err != nil {
+		slog.Error("fail to send response. err: %v", err)
 	}
 }
